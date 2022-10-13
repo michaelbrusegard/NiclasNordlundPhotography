fe85c22ac27588466ac5dd8cd1a32ce60b965d3b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2022-10-13 06:16:47 +0200

    Added mobile nav menu animation, streamlined nav.js code, added more variables to style.css

--- Site/scripts/nav.js
+++ Site/scripts/nav.js
@@ -1,17 +1,11 @@
 const linkMenu = document.querySelector('.linkMenu');
+const mobileNav = document.querySelector('.mobileNav');
 const desktopNav = document.querySelector('.desktopNav');
-let menuClicked = false;
 
 // Checks for a click on menu button
 
 linkMenu.addEventListener('click', () => {
-    if (!menuClicked) {
-        linkMenu.classList.add('clicked');
-        desktopNav.classList.add('active');
-        menuClicked = true;
-    } else {
-        linkMenu.classList.remove('clicked');
-        desktopNav.classList.remove('active');
-        menuClicked = false;
-    }
+    linkMenu.classList.toggle('clicked');
+    desktopNav.classList.toggle('active');
+    mobileNav.classList.toggle('active');
 }); 
