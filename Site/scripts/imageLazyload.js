5df00e36d177d179e50f51e95d0d903f1f58bb77
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2022-11-12 16:24:20 +0100

    Finished LazyLoad function, compressed wallpapers

--- Site/scripts/imageLazyload.js
+++ Site/scripts/imageLazyload.js
@@ -1,12 +1,20 @@
+// Function that creates image tags with lazyloading and adds them to the wrapper
+function lazyload(label, wrapper, end, start = 1) {
 
-function lazyload(label, wrapper) {
-  for (i = 1; i < 10; i++) { 
-      let image = document.createElement('img');
-      image.src = 'img/showcase/' + label + i + '.jpg';
-      image.alt = label;
-      image.loading = 'lazy';
-      image.width = image.width;
-      image.height = image.height;
-      wrapper.appendChild(image)
-    }
+  // Runs for the amount of pictures specified
+  for (i = start; i <= end; i++) {
+    let image = document.createElement('img');
+
+    // Loads image from showcase, sets alt and lazy
+    image.src = 'img/showcase/' + label + i + '.jpg';
+    image.alt = label;
+    image.loading = 'lazy';
+
+    // Sets the html height and width so the image takes the same amount of space before it is loaded
+    image.width = image.width;
+    image.height = image.height;
+
+    // Adds image to the wrapper
+    wrapper.appendChild(image);
+  }
 }
--- Site/scripts/showcase.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', weddingTest());
-
-function weddingTest() {
-    let wrap = document.getElementById('test')
-    lazyload('wedding', wrap)
-}
