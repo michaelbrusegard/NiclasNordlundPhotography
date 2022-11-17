25f6544c9eec57bd8ac0881e8b0124e946fa5922
martivj <martinvagseterjakobsen@yahoo.no>
2022-11-17 11:41:19 +0100

    checkout animation fixed!!!!

--- Site/scripts/checkoutSystem.js
+++ Site/scripts/checkoutSystem.js
@@ -18,14 +18,33 @@ function alignCheckout() {
     }
 }
 
-// WORKING CODE WITHOUT ANIMATION
 // Checkout system: add and remove items from the shop to the checkout menu
 function checkoutSystem(shopItem, itemPrice) {
     // Clone the shop item to a checkout item
     const checkoutItem = shopItem.cloneNode(true);
-    // Hide the shop item
-    shopItem.classList.toggle('hidden');
-    checkoutLoad();
+    // Calculate animation variables
+    rectItem = shopItem.getBoundingClientRect();
+    const itemX = (rectItem.left + rectItem.right) / 2;
+    const itemY = (rectItem.bottom + rectItem.top) / 2;
+    let cartX = 0;
+    let cartY = 0;
+    cartButtons.forEach(el => {
+        rectButton = el.getBoundingClientRect();
+        if (rectButton.left > 100) {
+            cartX += (rectButton.left + rectButton.right) / 2;
+            cartY += (rectButton.bottom + rectButton.top) / 2;
+        }
+    });
+    const x = cartX - itemX;
+    const y = cartY - itemY;
+    shopItem.style.setProperty('--addToCartX', `${x}px`);
+    shopItem.style.setProperty('--addToCartY', `${y}px`);
+    // Move shop item into cart
+    shopItem.classList.toggle('inCart')
+    shopItem.addEventListener('animationend', () => {
+        shopItem.style.display = 'none';
+        checkoutLoad();
+    }, {once: true})
     // Place the checkout item in the checkout menu
     checkoutItem.classList.add('checkout');
     checkoutMenu.prepend(checkoutItem);
@@ -35,105 +54,27 @@ function checkoutSystem(shopItem, itemPrice) {
     // Update checkout price
     checkoutTotal += itemPrice;
     checkoutTotalDisplay.innerHTML = `Total: ${checkoutTotal}€`;
-
-    // When a checkout item is removed:
+    // When a checkout item is clicked:
     checkoutItem.addEventListener('click', () => {
+        // Move shop item back into to the shop
+        shopItem.style.display = 'block';
+        shopItem.style.animationDirection = 'reverse';
+        shopItem.addEventListener('animationend', () => {
+            shopItem.classList.toggle('inCart');
+            //shopItem.style.zIndex = '0';
+            shopItem.style.animationDirection = 'none';
+            shopItem.style.animationDirection = 'normal';
+        }, {once: true});
         // Remove the checkout item
-        checkoutItem.remove();
-        // Make the shop item visible
-        shopItem.classList.toggle('hidden');
+        checkoutItem.classList.toggle('deleted');
+        checkoutItem.addEventListener('transitionend', () => {
+            checkoutItem.remove(); 
+        }, {once: true});
         // Update the red dot value
         itemNumber -= 1;
         redDots.forEach(el => el.innerHTML = itemNumber);
         // Update checkout price
         checkoutTotal -= itemPrice;
         checkoutTotalDisplay.innerHTML = `Total: ${checkoutTotal}€`;
-
-    });
+    }, {once: true});
 }
-
-/*
-// TESTING CODE WITH ANIMATION
-// Checkout system: add and remove items from the shop to the checkout menu
-function checkoutSystem(shopItem, itemPrice) {
-    // Clone the shop item to a checkout item
-    const checkoutItem = shopItem.cloneNode(true);
-    // Place the checkout item in the checkout menu
-    checkoutItem.classList.toggle('checkout');
-    checkoutMenu.prepend(checkoutItem);
-    // Update the red dot value
-    itemNumber += 1;
-    redDots.forEach(el => el.innerHTML = itemNumber);
-    // Update checkout price
-    checkoutTotal += itemPrice;
-    checkoutTotalDisplay.innerHTML = `Total: ${checkoutTotal}€`;
-
-    // Animation: calculate the transform parameters
-    rectItem = shopItem.getBoundingClientRect();
-    const itemX = (rectItem.left + rectItem.right) / 2;
-    const itemY = (rectItem.bottom + rectItem.top) / 2;
-    console.log(`x=${itemX}, y=${itemY}`);
-    let buttonX = 0;
-    let buttonY = 0;
-    cartButtons.forEach(el => {
-        rectButton = el.getBoundingClientRect();
-        if (rectButton.left > 100) {
-            buttonX += (rectButton.left + rectButton.right) / 2;
-            buttonY += (rectButton.bottom + rectButton.top) / 2;
-        }
-    });
-    const x = buttonX - itemX;
-    const y = buttonY - itemY;
-    
-    shopItem.classList.toggle('animation');
-
-    // Apply transform to shop item
-    shopItem.addEventListener('transitionrun', runAnimationOne());
-    shopItem.addEventListener('transitionend', endAnimationOne());
-    shopItem.style.transform=`translateX(${x}px) translateY(${y}px)`;
-    shopItem.removeEventListener('transitionrun', runAnimationOne);
-    shopItem.removeEventListener('transitionend', endAnimationOne);
-    
-    function runAnimationOne() {
-        shopItem.classList.toggle('animation');
-        console.log('animstart1');
-    }
-
-    function endAnimationOne() {
-        shopItem.classList.toggle('animation');
-        shopItem.classList.toggle('hidden');
-        checkoutLoad();
-        console.log('animend1');
-    }
-
-    // When a checkout item is removed:
-    checkoutItem.addEventListener('click', () => {
-        // Remove the checkout item
-        checkoutItem.remove();
-        // Apply reverse transform to shop item
-        shopItem.classList.toggle('animation');
-        shopItem.addEventListener('transitionrun', runAnimationTwo());
-        shopItem.addEventListener('transitionend', endAnimationTwo())
-        shopItem.style.transform=`translateX(${0}px) translateY(${0}px)`;
-        shopItem.removeEventListener('transitionrun', runAnimationTwo);
-        shopItem.removeEventListener('transitionend', endAnimationTwo)
-        
-        function runAnimationTwo() {
-            shopItem.classList.toggle('animation');
-            shopItem.classList.toggle('hidden');
-            console.log('animstart2');
-        }
-        
-        function endAnimationTwo() {
-            console.log('animend2');
-        }
-
-        // Update the red dot value
-        itemNumber -= 1;
-        redDots.forEach(el => el.innerHTML = itemNumber);
-        // Update checkout price
-        checkoutTotal -= itemPrice;
-        checkoutTotalDisplay.innerHTML = `Total: ${checkoutTotal}€`;
-    });
-}
-*/
