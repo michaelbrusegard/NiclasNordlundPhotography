9371b004f65cff710dbe47c5d358d3da643ef726
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2022-11-17 01:10:33 +0100

    Fixed the issue with replaying the bagIcon ani

--- Site/scripts/shopNav.js
+++ Site/scripts/shopNav.js
@@ -7,56 +7,61 @@ const linkCart = document.querySelectorAll('.linkCart');
 // Runs when the page is loaded
 document.addEventListener('DOMContentLoaded', () => {
     // Gets the link the page is navigated from
-    const navigatedFrom = document.referrer
+    let navigatedFrom = document.referrer;
+
+    // If the page is reloaded asigns navigatedFrom to something else to avoid playing the animation
+    if (getNavigationType() === 'reload') {
+        navigatedFrom = '';
+    }
 
     // Checks if the link that is navigated from is one of the other html pages
-    if (navigatedFrom.endsWith('home.html') || 
-    navigatedFrom.endsWith('nature.html') || 
-    navigatedFrom.endsWith('animals.html') || 
-    navigatedFrom.endsWith('architectural.html') || 
-    navigatedFrom.endsWith('portrait.html') || 
-    navigatedFrom.endsWith('sport.html') || 
-    navigatedFrom.endsWith('wedding.html')) {
+    if (navigatedFrom.endsWith('home.html') ||
+        navigatedFrom.endsWith('nature.html') ||
+        navigatedFrom.endsWith('animals.html') ||
+        navigatedFrom.endsWith('architectural.html') ||
+        navigatedFrom.endsWith('portrait.html') ||
+        navigatedFrom.endsWith('sport.html') ||
+        navigatedFrom.endsWith('wedding.html')) {
 
         // Runs the animation for the old navigation bar
-        let oldNav = mobileOrDesktopNav() + 2
-        linkBag[oldNav].classList.add('linkBagAnimation')
-        linkSignature[oldNav].classList.add('fadeOutAnimation')
+        let oldNav = mobileOrDesktopNav() + 2;
+        linkBag[oldNav].classList.add('linkBagAnimation');
+        linkSignature[oldNav].classList.add('fadeOutAnimation');
 
         // Checks different if it is desktop or mobile to know which elements need animation
         if (oldNav === 2) {
-            linkMenu.classList.add('fadeOutAnimation')
+            linkMenu.classList.add('fadeOutAnimation');
         } else {
             // Go through the text elements in the desktop navigation bar
             linkText.forEach((link) => {
                 // Set it to visible before running the animation (It is invisible to stop it from showing up in preloading)
-                link.style.visibility = 'visible'
-                link.classList.add('fadeOutAnimation')
-            })
+                link.style.visibility = 'visible';
+                link.classList.add('fadeOutAnimation');
+            });
         }
     } else {
         // Refresh Navigation bar sicne it is not coming from one of the other html pages and the animation is cancelled
-        refreshNav()
+        refreshNav();
     }
 });
 
 // When the bag icon animation end
 linkBag[mobileOrDesktopNav() + 2].addEventListener('animationend', () => {
     // Replaces the navigation bar
-    refreshNav()
-    let newNav = mobileOrDesktopNav()
+    refreshNav();
+    let newNav = mobileOrDesktopNav();
     // Initiates fade in animation for the elements in the new navigation bar
-    linkSignature[newNav].classList.add('fadeInAnimation')
-    linkCart[newNav].classList.add('fadeInAnimation')
-}); 
+    linkSignature[newNav].classList.add('fadeInAnimation');
+    linkCart[newNav].classList.add('fadeInAnimation');
+});
 
 // Runs when the cart animation is done aka when all animations are completely done
 linkCart[mobileOrDesktopNav()].addEventListener('animationend', () => {
-    let newNav = mobileOrDesktopNav()
+    let newNav = mobileOrDesktopNav();
     // Removes the fade in animation so it does not run unintentionally
-    linkSignature[newNav].classList.remove('fadeInAnimation')
-    linkCart[newNav].classList.remove('fadeInAnimation')
-}); 
+    linkSignature[newNav].classList.remove('fadeInAnimation');
+    linkCart[newNav].classList.remove('fadeInAnimation');
+});
 
 // Gets the navigation bars as variables
 const oldNav = document.getElementsByClassName('oldNav')[0];
@@ -71,14 +76,23 @@ function refreshNav() {
 // Function that gets if the anvigation bar is mobile or desktop
 function mobileOrDesktopNav() {
     // Gets the css variable for mobile size
-    const style = getComputedStyle(document.body)
+    const style = getComputedStyle(document.body);
     const mobileNavSize = parseInt(style.getPropertyValue('--mobileNavSize').slice(0, -2));
     // Gets the sreen width
-    const screenWidth = window.innerWidth
+    const screenWidth = window.innerWidth;
     // Compares screen width to the mobile size
     if (mobileNavSize > screenWidth) {
-        return 0
+        return 0;
     } else {
-        return 1
+        return 1;
     }
+}
+
+// Function to get the navigation type
+function getNavigationType() {
+    let type = '';
+    performance.getEntriesByType('navigation').forEach((p) => {
+        type = p.type;
+    });
+    return type;
 }
