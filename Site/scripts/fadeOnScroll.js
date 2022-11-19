00bdd0d67ad5b10cb21df96d8c57e45450664021
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2022-11-19 03:49:57 +0100

    Worked on home

--- Site/scripts/fadeOnScroll.js
+++ Site/scripts/fadeOnScroll.js
@@ -1,74 +1,74 @@
 // Adds fade on scroll effect in an observer
-function showcaseFadeOnscroll(entries) {
-    // Goes over every entry that is observed
-    entries.map((entry) => {
-        // Gets columnx and the entry parent variable
+function showcaseFadeOnscroll(items) {
+    // Goes over every item that is observed
+    items.map((item) => {
+        // Gets columnx and the item parent variable
         const columns = document.getElementsByClassName('column');
         const columnsAmount = columns.length;
-        const parent = entry.target.parentNode;
+        const parent = item.target.parentNode;
 
-        // Checks if the entry is intersecting
-        if (entry.isIntersecting) {
+        // Checks if the item is intersecting
+        if (item.isIntersecting) {
             // Checks how many columns there are
             // Adds the fade in animation based on which column the image is
             if (columnsAmount === 1) {
-                entry.target.classList.add('fadeInFromBottom');
+                item.target.classList.add('fadeInFromBottom');
             } else if (columnsAmount === 2) {
                 if (parent === columns[0]) {
-                    entry.target.classList.add('fadeInFromLeftBottom');
+                    item.target.classList.add('fadeInFromLeftBottom');
                 } else {
-                    entry.target.classList.add('fadeInFromRightBottom');
+                    item.target.classList.add('fadeInFromRightBottom');
                 }
             } else if (columnsAmount === 3) {
                 if (parent === columns[0]) {
-                    entry.target.classList.add('fadeInFromLeftBottom');
+                    item.target.classList.add('fadeInFromLeftBottom');
                 } else if (parent === columns[1]) {
-                    entry.target.classList.add('fadeInFromBottom');
+                    item.target.classList.add('fadeInFromBottom');
                 } else {
-                    entry.target.classList.add('fadeInFromRightBottom');
+                    item.target.classList.add('fadeInFromRightBottom');
                 }
             } else if (columnsAmount === 4) {
                 if (parent === columns[0]) {
-                    entry.target.classList.add('fadeInFromLeft');
+                    item.target.classList.add('fadeInFromLeft');
                 } else if (parent === columns[1]) {
-                    entry.target.classList.add('fadeInFromLeftBottom');
+                    item.target.classList.add('fadeInFromLeftBottom');
                 } else if (parent === columns[2]) {
-                    entry.target.classList.add('fadeInFromRightBottom');
+                    item.target.classList.add('fadeInFromRightBottom');
                 } else {
-                    entry.target.classList.add('fadeInFromRight');
+                    item.target.classList.add('fadeInFromRight');
                 }
             } else if (columnsAmount === 5) {
                 if (parent === columns[0]) {
-                    entry.target.classList.add('fadeInFromLeft');
+                    item.target.classList.add('fadeInFromLeft');
                 } else if (parent === columns[1]) {
-                    entry.target.classList.add('fadeInFromLeftBottom');
+                    item.target.classList.add('fadeInFromLeftBottom');
                 } else if (parent === columns[2]) {
-                    entry.target.classList.add('fadeInFromBottom');
+                    item.target.classList.add('fadeInFromBottom');
                 } else if (parent === columns[3]) {
-                    entry.target.classList.add('fadeInFromRightBottom');
+                    item.target.classList.add('fadeInFromRightBottom');
                 } else {
-                    entry.target.classList.add('fadeInFromRight');
+                    item.target.classList.add('fadeInFromRight');
                 }
             } else {
                 if (parent === columns[0]) {
-                    entry.target.classList.add('fadeInFromLeft');
+                    item.target.classList.add('fadeInFromLeft');
                 } else if (parent === columns[1]) {
-                    entry.target.classList.add('fadeInFromLeftBottom');
+                    item.target.classList.add('fadeInFromLeftBottom');
                 } else if (parent === columns[columnsAmount - 2]) {
-                    entry.target.classList.add('fadeInFromRightBottom');
+                    item.target.classList.add('fadeInFromRightBottom');
                 } else if (parent === columns[columnsAmount - 1]) {
-                    entry.target.classList.add('fadeInFromRight');
+                    item.target.classList.add('fadeInFromRight');
                 } else {
-                    entry.target.classList.add('fadeInFromBottom');
+                    item.target.classList.add('fadeInFromBottom');
                 }
             }
         } else {
             // Removes any fade in animation when not intersecting
-            entry.target.classList.remove('fadeInFromBottom');
-            entry.target.classList.remove('fadeInFromLeft');
-            entry.target.classList.remove('fadeInFromRight');
-            entry.target.classList.remove('fadeInFromLeftBottom');
-            entry.target.classList.remove('fadeInFromRightBottom');
+            item.target.classList.remove('fadeInFromBottom');
+            item.target.classList.remove('fadeInFromLeft');
+            item.target.classList.remove('fadeInFromRight');
+            item.target.classList.remove('fadeInFromLeftBottom');
+            item.target.classList.remove('fadeInFromRightBottom');
         }
     });
 }
