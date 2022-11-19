2655f2477245959f09e3226c5cd800d34c396d2b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2022-11-20 00:57:23 +0100

    Added comments and moved variables into home.js

--- Site/scripts/imageCarousel.js
+++ Site/scripts/imageCarousel.js
@@ -1,47 +1,56 @@
+// Function to run the carousel
 function imageCarousel() {
-    const daddy = document.querySelector('#imageDaddy')
-    const images = document.querySelectorAll('#imageDaddy img');
-    const intervalTime = 5000
-
-    newImage(images, imageDisplayed)
-    let timer = setInterval(function() {autoImage(images)}, intervalTime);
-
-    daddy.addEventListener('click', () => {
-        clearInterval(timer)
-        autoImage(images)
-        timer = setInterval(function() {autoImage(images)}, intervalTime);
-    })
+    // Display the first image and start the timer
+    newImage(imageDisplayed);
+    let timer = setInterval(function () { autoImage(); }, intervalTime);
+    // Checking for clicks on image daddy
+    imageDaddy.addEventListener('click', () => {
+        // Restarting the timer and runnign the image swapping function
+        clearInterval(timer);
+        autoImage();
+        timer = setInterval(function () { autoImage(); }, intervalTime);
+    });
 }
 
-function autoImage(images) {
-    removeImage(images, imageDisplayed)
-        imageDisplayed += 1
-        if (imageDisplayed >= images.length) {
-            imageDisplayed = 0
-        }
-        newImage(images, imageDisplayed)
+// Function to swap images
+function autoImage() {
+    // Removes current image
+    removeImage(imageDisplayed);
+    // Sets the next image
+    imageDisplayed += 1;
+    if (imageDisplayed >= images.length) {
+        imageDisplayed = 0;
+    }
+    // Displays new image
+    newImage(imageDisplayed);
 }
 
-function newImage(images, index) {
+// Displaying a new image
+function newImage(index) {
+    // Mark the image as visible and toggle animation
     images[index].style.visibility = "visible";
     images[index].classList.add("imageIn");
+    // Remove animation after it is finished running
     images[index].addEventListener("animationend", () => {
         images[index].classList.remove("imageIn");
-    }, {once: true})
+    }, { once: true });
 }
 
-function removeImage(images, index) {
+function removeImage(index) {
+    // Toggle the out animation
     images[index].classList.add("imageOut");
+    // When animation is done set it to hidden and remove animation
     images[index].addEventListener("animationend", () => {
         images[index].classList.remove("imageOut");
         images[index].style.visibility = "hidden";
-    }, {once: true})
+    }, { once: true });
 }
 
+// Function to adjust the images position
 function imagePosition() {
-    const images = document.querySelectorAll('#imageDaddy img');
-
+    // For every image
     images.forEach(image => {
+        // Change the padding based on if it is portrait or landscape
         if (isPortraitOrientation()) {
             image.style.paddingLeft = "2%";
             image.style.paddingRight = "4%";
