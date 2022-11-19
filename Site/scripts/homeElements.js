930e79df09475b0a1addf3cd17436de82ffe34a3
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2022-11-20 00:37:26 +0100

    Created carousell with timer

--- Site/scripts/homeElements.js
+++ Site/scripts/homeElements.js
@@ -59,6 +59,7 @@ window.addEventListener("scroll", () => {
 // Writes the initial text and sets a high scrollY on DOM load
 document.addEventListener('DOMContentLoaded', () => {
     window.scrollTo(0, scrollMaxValue() - scrollDistance);
+    imageDaddy.classList.toggle("hidden");
     writeText();
 })
 
