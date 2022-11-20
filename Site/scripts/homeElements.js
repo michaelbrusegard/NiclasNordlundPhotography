ab7cec3d7c4328d9c4ae57c5e158134af24644c5
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2022-11-20 16:36:26 +0100

    Added text to functions in homeelementz

--- Site/scripts/homeElements.js
+++ Site/scripts/homeElements.js
@@ -16,6 +16,7 @@ async function writeText(element, quoteIndex) {
     }
 }
 
+// Creates observers for home elements and starts observing them
 function observeHome() {
     const carouselObserver = new IntersectionObserver(observeCarousel, { threshold: 0.5 });
     const quotesObserver = new IntersectionObserver(observeQuotes, { threshold: 0.5 });
@@ -25,6 +26,7 @@ function observeHome() {
     }
 }
 
+// Observing the carousel and adding/removing hidden class based on if it is intersecting
 function observeCarousel(items) {
     items.map((item => {
         if (item.isIntersecting) {
@@ -35,12 +37,14 @@ function observeCarousel(items) {
     }));
 }
 
+// Observing quotes and adding/removing hidden class based on if it is intersecting 
 function observeQuotes(items) {
     items.map((item => {
         if (item.isIntersecting) {
             item.target.classList.remove('hidden');
             for (i = 0; i < quotesDaddy.length; i++) {
                 if (item.target === quotesDaddy[i]) {
+                    // Start writing text
                     writeText(quoteElements[i], i);
                 }
             }
@@ -50,6 +54,7 @@ function observeQuotes(items) {
     }));
 }
 
+// Adjust the text position based on screen width and orientation
 function textPosition() {
     for (let i = 0; i < quoteElements.length; i++) {
         if (isPortraitOrientation()) {
@@ -68,8 +73,9 @@ function textPosition() {
     }
 }
 
+// Adjust the element height based on innerheight minus navbar and footer
 function elementsPosition() {
-    const containerHeight = String(window.innerHeight - 2 * parseInt(style.getPropertyValue('--navContainerHeight').slice(0, -2)) + 'px')
+    const containerHeight = String(window.innerHeight - 2 * parseInt(style.getPropertyValue('--navContainerHeight').slice(0, -2))) + 'px';
     imageDaddy.style.height = containerHeight
     console.log(containerHeight)
     for (let i = 0; i < quotesDaddy.length; i++) {
