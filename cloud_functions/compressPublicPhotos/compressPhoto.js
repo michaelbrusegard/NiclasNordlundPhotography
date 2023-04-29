52f8cdc28a93d97d4f5ea98b45819b68209cd98d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-04-30 01:40:17 +0200

    Added deletePhoto cloud function

--- cloud_functions/compressPublicPhotos/compressPhoto.js
+++ cloud_functions/compressPublicPhotos/compressPhoto.js
@@ -4,7 +4,6 @@ const { pipeline } = require('stream');
 const { promisify } = require('util');
 const sharp = require('sharp');
 
-// Replace these values with your own
 const SOURCE_BUCKET_NAME = process.env.GCLOUD_PHOTOS_BUCKET;
 const DESTINATION_BUCKET_NAME = process.env.GCLOUD_PUBLIC_PHOTOS_BUCKET;
 const MAX_WIDTH = 1024; // The maximum width of the low-quality photos
@@ -13,27 +12,27 @@ const QUALITY = 60; // The desired quality for the low-quality photos
 
 const storage = new Storage();
 
-exports.compressPhoto = async (event, context) => {
-  const file = event;
-  const sourceBucket = storage.bucket(file.bucket);
-  const destinationBucket = storage.bucket(DESTINATION_BUCKET_NAME);
+exports.compressPhoto = async (event) => {
+    const file = event;
+    const sourceBucket = storage.bucket(file.bucket);
+    const destinationBucket = storage.bucket(DESTINATION_BUCKET_NAME);
 
-  const sourceFile = sourceBucket.file(file.name);
-  const destinationFile = destinationBucket.file(file.name);
+    const sourceFile = sourceBucket.file(file.name);
+    const destinationFile = destinationBucket.file(file.name);
 
-  const sourceStream = sourceFile.createReadStream();
-  const destinationStream = destinationFile.createWriteStream();
+    const sourceStream = sourceFile.createReadStream();
+    const destinationStream = destinationFile.createWriteStream();
 
-  // Use Sharp to convert the photo to low quality and resize it
-  const transformStream = sharp()
-    .jpeg({ quality: QUALITY })
-    .resize(MAX_WIDTH, MAX_HEIGHT, {
-      fit: 'inside', // Resize only if the photo is larger than the maximum size
-      withoutEnlargement: true, // Do not enlarge the photo if it is smaller than the maximum size
-    });
+    // Use Sharp to convert the photo to low quality and resize it
+    const transformStream = sharp()
+        .jpeg({ quality: QUALITY })
+        .resize(MAX_WIDTH, MAX_HEIGHT, {
+            fit: 'inside', // Resize only if the photo is larger than the maximum size
+            withoutEnlargement: true, // Do not enlarge the photo if it is smaller than the maximum size
+        });
 
-  // Use pipeline to stream the photo through Sharp and upload to destination bucket
-  await promisify(pipeline)(sourceStream, transformStream, destinationStream);
+    // Use pipeline to stream the photo through Sharp and upload to destination bucket
+    await promisify(pipeline)(sourceStream, transformStream, destinationStream);
 
-  return destinationFile;
+    return destinationFile;
 };
