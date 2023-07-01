1f8e8ba950ef63519fe4e3abb94eabac75b985a0
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-07-01 21:54:37 +0200

    test: :bug: find photo with name bug

--- website/public/scripts/shop.js
+++ website/public/scripts/shop.js
@@ -13,6 +13,7 @@ async function getPricesArray(publicPhotosBucket) {
     const data = await response.json();
     const pricesArray = data.items.map((item) => {
         const name = item.name;
+        console.log(name);
         const price = parseInt(name.match(/\d+(?=e\.jpg$)/i)[0]);
         return [name, price];
     });
