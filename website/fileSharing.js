b6d5ec818c8a27098cbd7edbfce559b306086470
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-07-01 15:33:51 +0200

    test: :rocket: deploy with app.yaml

--- website/fileSharing.js
+++ website/fileSharing.js
@@ -24,7 +24,6 @@ async function handlePhotos(purchasedItems) {
         );
 
         console.log(`Zip file URL: ${response.data.url}`);
-        console.log(`Zip file name: ${response.data.name}`);
     } catch (error) {
         console.error(`Failed to zip photos: ${error}`);
     }
