27dbcad607239364dc3e1ad9e300851aa9814ccd
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-09-27 13:53:52 +0200

    test: deploy website with colud build service account

--- website/fileSharing.js
+++ website/fileSharing.js
@@ -20,13 +20,14 @@ async function handlePhotos(purchasedItems) {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
-            },
+            }
         );
 
         console.log(`Zip file URL: ${response.data.url}`);
         console.log(`Zip file name: ${response.data.name}`);
     } catch (error) {
         console.error(`Failed to zip: ${error}`);
+        console.log(`Failed to zip: ${error}`);
     }
 }
 
