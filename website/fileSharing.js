e4a5a55d1b2ce35dab91fa35322958d334313d9b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-09-28 06:39:26 +0200

    feat: worked on atuhentication testing

--- website/fileSharing.js
+++ website/fileSharing.js
@@ -7,7 +7,9 @@ async function handlePhotos(purchasedItems) {
         process.env.ARCHIVE_PURCHASED_PHOTOS_FUNCTION_URL;
 
     try {
-        const auth = new GoogleAuth();
+        const auth = new GoogleAuth({
+            scopes: ["https://www.googleapis.com/auth/cloud-platform"],
+        });
         const client = await auth.getClient();
         const token = await client.getAccessToken();
 
@@ -20,14 +22,28 @@ async function handlePhotos(purchasedItems) {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
-            },
+            }
         );
 
         console.log(`Zip file URL: ${response.data.url}`);
         console.log(`Zip file name: ${response.data.name}`);
+
+        return {
+            status: 200,
+            body: {
+                message: "Zip file created successfully.",
+                url: response.data.url,
+            },
+        };
     } catch (error) {
         console.error(`Failed to zip: ${error}`);
-        console.error(error.response.data);
+
+        return {
+            status: 500,
+            body: {
+                error: error.message,
+            },
+        };
     }
 }
 
